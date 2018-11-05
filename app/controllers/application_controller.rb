5cb981cc38cbada848f9d1d1625608c4cba85e07
Chris Franklin <cf73021@gmail.com>
2018-11-05 18:12:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  patch '/recipes/:id/edit' do
+  post '/recipes/:id/edit' do
     @recipe = Recipe.find(params[:id])
     erb :edit
   end
